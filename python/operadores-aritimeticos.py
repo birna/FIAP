d396a35a1b899c1b71ca3f411a3f50930b343aee
Guilherme Urbinatti <guilherme.urbinatti@hotmail.com>
2025-03-11 12:35:20 +0000

    operadores

---
+++ python/operadores-aritimeticos.py
@@ -0,0 +1,43 @@
+#OPERADORES ARITIMETICOS
+"""
+	() - Parenteses
+	** - Exponenciacao
+	*  - Multiplicacao
+	/  - Divisao
+	// - Divisao Inteira
+	%  - Resto da divisao
+	+  - Adicao
+	-  - Subtracao
+"""
+
+a = 2
+b = 5
+total = a ** b
+print(f"A= %f ** B= %f \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a * b
+print(f"A= %f * B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a / b
+print(f"A= %f / B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a // b
+print(f"A= %f // B= %f \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a % b
+print(f"A= %f  B= %f   \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a + b
+print(f"A= %f + B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+total = a - b
+print(f"A= %f - B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
