115529f40a0c45d209ebac26d5c3943fb6181432
Guilherme Urbinatti <guilherme.urbinatti@hotmail.com>
2025-03-11 18:00:42 +0000

    maiorNumero.py

---
+++ python/maiorNumero.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+if num1 > num2:
+	print(f"Maior numero: {num1}")
+else:
+	print(f"Maior numero: {num2}")
