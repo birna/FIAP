6e3f25d7a384496278096bba2a805ac4cc5bf3a3
Guilherme Urbinatti <guilherme.urbinatti@hotmail.com>
2025-03-11 13:10:57 +0000

    equacoes

--- python/operadores-aritimeticos.py
+++ python/operadores-aritimeticos.py
@@ -18,14 +18,14 @@ print(f"A= %f ** B= %f \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,t
 total = a * b
 print(f"A= %f * B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
 
-total = a / b
+total = b / a
 print(f"A= %f / B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
 
-total = a // b
+total = b // a
 print(f"A= %f // B= %f \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
 
-total = a % b
-print(f"A= %f  B= %f   \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
+total = b % a
+print(f"A= %f %%  B= %f   \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
 
 total = a + b
 print(f"A= %f + B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
@@ -33,11 +33,37 @@ print(f"A= %f + B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,t
 total = a - b
 print(f"A= %f - B= %f  \n-----------------------------\nTOTAL= %f\n\n"  % (a,b,total))
 
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
-#print(f"A= %f ** B= %f \n--------------\nTOTAL= %f\n\n"  % (a,b,total))
+
+print(">>>>> EXEMPLOS AULA <<<<<\n\n")
+a = 10
+b = 3
+c = 2
+
+total = b ** c
+print(f"A=%d B=%d C=%d \nFORMULA: b ** c\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+
+total = a + b * c
+print(f"A=%d B=%d C=%d \nFORMULA: a + b * c\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = (a + b) * c
+print(f"A=%d B=%d C=%d \nFORMULA: (a + b) * c\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+
+total = a + b + c / 3
+print(f"A=%d B=%d C=%d \nFORMULA: a + b + c / 3\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = a % b
+print(f"A=%d B=%d C=%d \nFORMULA: a %% b\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = c - b % a
+print(f"A=%d B=%d C=%d \nFORMULA: c - b %% a\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = a / b
+print(f"A=%d B=%d C=%d \nFORMULA: a / b\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = a // b
+print(f"A=%d B=%d C=%d \nFORMULA: a // b\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
+
+total = a + b % c + c // b
+print(f"A=%d B=%d C=%d \nFORMULA: a + b %% c + c // b\n-----------------------------------\nTOTAL= %10.2f\n\n" % (a,b,c,total))
